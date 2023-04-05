89ad44429208e849966185a2e883269088fa3493
mrrsmburu <mrrsmburu@gmail.com>
2023-04-05 22:13:32 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const hq = 42;
+    let distanceInBlocks = Math.abs(hq - location);
+    return distanceInBlocks;
+  }
+  
+  function distanceFromHqInFeet(location) {
+    const blockInFeet = 264;
+    let distanceInFeet = distanceFromHqInBlocks(location) * blockInFeet;
+    return distanceInFeet;
+  }
+  
+  function distanceTravelledInFeet(start, end) {
+    const blockInFeet = 264;
+    let distanceInFeet = Math.abs(start - end) * blockInFeet;
+    return distanceInFeet;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const freeFeet = 400;
+    const maxFeetWithoutExtraCharge = 2000;
+    const extraChargePerFoot = 0.02;
+    const maxFeetWithoutCharge = 2500;
+  
+    let distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+    if (distanceInFeet <= freeFeet) {
+      return 0;
+    } else if (distanceInFeet <= maxFeetWithoutExtraCharge) {
+      return (distanceInFeet - freeFeet) * extraChargePerFoot;
+    } else if (distanceInFeet <= maxFeetWithoutCharge) {
+      return 25;
+    } else {
+      return 'cannot travel more than 2500 feet';
+    }
+  }
